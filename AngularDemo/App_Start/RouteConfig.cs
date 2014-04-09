8caff7f64f406dbdec37bc94904f114d2a8c54fc
nouellette <neil.158@gmail.com>
2014-04-09 08:20:13 -0400

    Angular Demo application

---
+++ AngularDemo/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace AngularDemo
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Api",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Api", action = "GetData", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
